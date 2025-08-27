a64009e1cd80278e2181cca44c95b50ef755d3fc
moses <moses.fade01@gmail.com>
2025-08-27 18:46:13 +0800

    Add files via upload

---
+++ coding_challenge3.py
@@ -0,0 +1,11 @@
+username = 'moses113'
+password = '123456789'
+
+uname = input("Input your username ---> ")
+p_word = input("Enter your password ---> ")
+
+if username == uname and password == p_word:
+	print("Access Granted")
+
+else:
+	print("Access Denied")
