bb05c167cfcb7f7e7af63b438e788e584bc1f92e
moses <moses.fade01@gmail.com>
2025-09-17 12:46:26 +0800

    Add files via upload

---
+++ code_challenge9.py
@@ -0,0 +1,13 @@
+#create a countdown timer simulator 
+#simulate a simple countdown timer using for loop that counts from a user-specified number down to 1, 
+#then prints "Liftoff!"
+
+
+print("COUNTDOWN TIMER SIMULATOR")
+
+cd = eval(input("Enter the starting number for countdown:" ))
+print("\nCountdown Started:")
+
+for fern in range(cd, 0, -1):
+    print(fern)
+print("Liftoff!")
