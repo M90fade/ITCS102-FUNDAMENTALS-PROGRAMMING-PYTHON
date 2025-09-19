89cf601fd6e6336f2cd731327231eb626387cf5b
moses <moses.fade01@gmail.com>
2025-09-20 06:00:10 +0800

    Add files via upload

---
+++ activity15.py
@@ -0,0 +1,17 @@
+#String Formatting
+
+n1 = 'Moses'
+n2 = 'Doctor'
+n3 = 'Faderagao'
+
+print(f"Hello {n1} {n2} {n3}, to the World")
+
+mi = 0
+
+for mae in range(1, 11, 1):
+    ma = eval(input(f"{mae} - Enter a number --> "))
+
+    if mae % 2 == 1:
+        mi += maess
+
+print(f"The total of odd number is: {mi}")
