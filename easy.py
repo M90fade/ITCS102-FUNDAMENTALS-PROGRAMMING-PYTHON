09b6971bf914a6da086d5f055caa899116b6a3c6
moses <moses.fade01@gmail.com>
2025-10-13 19:24:05 +0800

    Add files via upload

---
+++ easy.py
@@ -0,0 +1,12 @@
+#pyramid
+for i in range(1, 7, 1):
+    for x in range(7, i, -1):
+        print(" ", end="")
+    for y in range(1, i*2, 1):
+        print("*", end="")
+    print()
+#number pattern
+for z in range(1, 6, 1):
+    for v in range(1, z+1, 1):
+        print(z, end="")
+    print()
