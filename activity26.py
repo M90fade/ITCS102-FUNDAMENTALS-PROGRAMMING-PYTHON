79d9a7deed59c2a90a42f0a6aac4d9129f62c671
moses <moses.fade01@gmail.com>
2025-11-14 14:51:11 +0800

    Add files via upload

---
+++ activity26.py
@@ -0,0 +1,6 @@
+programming_language = ['Python', 'TurboC', 'C++', 'JavaScript', 'Perl', 'Java']
+
+#dictionary
+programming_language = {'easy':'Python','medyo luma na':'TurboC','mahirap':'C++','mas mahirap':'JavaScript','medyo mahirap':'Perl','napaka hirap':'Java'}
+
+print(programming_language['mas mahirap'])
---
+++ activity27.py
@@ -0,0 +1,41 @@
+print("Welcome to my dictionary")
+print("==========================")
+
+tuloy = True
+
+empty_dictionary = {}
+
+def print_anime():
+    for i,j in empty_dictionary.items():
+        print(f"CODE {i} TITLE -- {j}")
+
+def pang_search(key):
+    print("Searching....")
+    print(f"result shows {empty_dictionary[key]} on our database")
+
+while tuloy == True:
+    keys = input("Input keys for this anime --> ")
+    value = input("Enter the anime title --> ")
+
+    #adding data to a dictionary
+    empty_dictionary[keys] = value
+
+    choice = input("Would you like to continue adding anime \ny-Yes\nn-No\np-PRINT\ns-SEARCH\n-->").lower()
+
+    if choice == 'y':
+        print("Continuing...")
+        continue
+    elif choice == 'n':
+        print("Program Stops")
+        break
+    elif choice == 'p':
+        print_anime()
+        continue
+    elif choice == 's':
+        code = input("Please input the code of the anime ")
+        pang_search(code)
+        continue
+    else:
+        print("Invalid Choice")
+        continue
+        
